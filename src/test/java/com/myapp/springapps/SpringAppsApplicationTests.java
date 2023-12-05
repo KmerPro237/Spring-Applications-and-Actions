1ba6502357bc00b0eb9ece69a1bd21c6072baa6f
KmerPro237 <fotsososthene02@gmail.com>
2023-12-05 12:21:10 +0100

    new project resolving dependency conflict

---
+++ src/test/java/com/myapp/springapps/SpringAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
