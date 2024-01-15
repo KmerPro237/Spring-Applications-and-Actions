a03fb5663d15d11530ed48f2cb05e6358689f65f
KmerPro237 <fotsososthene02@gmail.com>
2024-01-15 11:36:33 +0100

    class Ãpath issues and project run configurations resolved

--- Mongodb/MongoProject/src/main/java/SostheneMongo/MongoProject/MongoProjectApplication.java
+++ Mongodb/MongoProject/src/main/java/SostheneMongo/MongoProject/MongoProjectApplication.java
@@ -1,11 +1,10 @@
-package SostheneMongo.MongoProject;
+package main.java.SostheneMongo.MongoProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongoProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MongoProjectApplication.class, args);
 	}
--- Mongodb/MongoProject/src/test/java/SostheneMongo/MongoProject/MongoProjectApplicationTests.java
+++ Mongodb/MongoProject/src/test/java/SostheneMongo/MongoProject/MongoProjectApplicationTests.java
@@ -1,6 +1,6 @@
-package SostheneMongo.MongoProject;
+package test.java.SostheneMongo.MongoProject;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
