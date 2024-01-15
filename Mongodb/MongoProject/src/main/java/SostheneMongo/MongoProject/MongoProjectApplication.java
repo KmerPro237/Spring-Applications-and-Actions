d66d941c58b817ec96f9f0794ef4fb62e99eaaf0
KmerPro237 <fotsososthene02@gmail.com>
2024-01-15 20:24:03 +0100

    changes

--- Mongodb/MongoProject/src/main/java/SostheneMongo/MongoProject/MongoProjectApplication.java
+++ Mongodb/MongoProject/src/main/java/SostheneMongo/MongoProject/MongoProjectApplication.java
@@ -1,4 +1,4 @@
-package main.java.SostheneMongo.MongoProject;
+package SostheneMongo.MongoProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
